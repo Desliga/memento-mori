7f716f39a9f232f8dedfa43dc739b8488f3fef8e
Naoko <naoko.replicant@gmail.com>
2024-12-09 22:05:59 -0300

    caixinha do capeta morram todos

--- MementoMori/Assets/Game/Scenes/Bedroom/Scripts/SongboxController.cs
+++ MementoMori/Assets/Game/Scenes/Bedroom/Scripts/SongboxController.cs
@@ -1,14 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using Game.Scenes.Bedroom.Scripts;
 using UnityEngine;
 
-public class SongboxController : MonoBehaviour
+public class SongboxController : MonoBehaviour, ILanternInteractable
 {
     private Animator animator;
     public GameObject enemy;
     public Animator enemyanim;
     public Transform enemypoint;
 
+    private bool canClear = false;
+    private Coroutine closeCoroutine;
+
+    private int clearCount = 0;
+    
     private void Awake()
     {
         animator = GetComponent<Animator>();
@@ -17,12 +23,15 @@ public class SongboxController : MonoBehaviour
     public void OpenSongbox()
     {
         animator.SetTrigger("open");
-        SoundManager.Instance.PlayKillerSong();
-        Invoke("CloseSongbox", 15f);
+        closeCoroutine = StartCoroutine(CloseCoroutine());
     }
 
-    private void CloseSongbox()
+    private IEnumerator CloseCoroutine()
     {
+        SoundManager.Instance.PlayKillerSong();
+        yield return new WaitForSeconds(5.0f);
+        canClear = true;
+        yield return new WaitForSeconds(10.0f);
         animator.SetTrigger("close");
         StartCoroutine(KillPlayer());
     }
@@ -35,5 +44,37 @@ public class SongboxController : MonoBehaviour
         SoundManager.Instance.PlayScare();
         yield return new WaitForSeconds(2.0f);
         GameManager.Instance.GameOver();
+    }
+
+    public void Interact()
+    {
+        if (canClear)
+        {
+            canClear = false;
+            SoundManager.Instance.songbox.Stop();
+            clearCount++;
+            if (clearCount == 3)
+            {
+                SoundManager.Instance.PlayBreak();
+                SoundManager.Instance.Rewind();
+                StopCoroutine(closeCoroutine);
+                Invoke(nameof(Rewind), 0.8f);
+            }
+            else if (clearCount == 5)
+            {
+                StartCoroutine(KillPlayer());
+            }
+            else
+            {
+                SoundManager.Instance.Rewind();
+                StopCoroutine(closeCoroutine);
+                Invoke(nameof(Rewind), 0.8f);
+            }
+        }
+    }
+
+    private void Rewind()
+    {
+        closeCoroutine = StartCoroutine(CloseCoroutine());
     }
 }
